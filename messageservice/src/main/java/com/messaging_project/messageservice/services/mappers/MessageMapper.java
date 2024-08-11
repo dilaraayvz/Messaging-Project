560ab2ef41a944e6169bfc2020f5236203e6066b
dilara <yavuz.dilara@outlook.com.tr>
2024-08-11 19:38:39 +0300

    Some fixes about messageservice.

---
+++ messageservice/src/main/java/com/messaging_project/messageservice/services/mappers/MessageMapper.java
@@ -0,0 +1,15 @@
+package com.messaging_project.messageservice.services.mappers;
+
+import com.messaging_project.messageservice.entities.Message;
+import com.messaging_project.messageservice.services.dtos.requests.MessageRequest;
+import com.messaging_project.messageservice.services.dtos.responses.MessageResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+@Mapper
+public interface MessageMapper {
+    MessageMapper INSTANCE = Mappers.getMapper(MessageMapper.class);
+
+    Message toEntity(MessageRequest messageRequest);
+
+    MessageResponse toDTO(Message message);
+}
