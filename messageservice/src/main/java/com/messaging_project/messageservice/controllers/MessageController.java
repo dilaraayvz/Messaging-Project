e18b0734d24e455d9d8267000f2c1fd306c8ae2e
dilara <yavuz.dilara@outlook.com.tr>
2024-08-15 22:14:12 +0300

    15.08.2024

--- messageservice/src/main/java/com/messaging_project/messageservice/controllers/MessageController.java
+++ messageservice/src/main/java/com/messaging_project/messageservice/controllers/MessageController.java
@@ -1,27 +1,28 @@
 package com.messaging_project.messageservice.controllers;
 
-import com.messaging_project.messageservice.entities.Message;
 import com.messaging_project.messageservice.services.abstracts.MessageService;
 import com.messaging_project.messageservice.services.dtos.requests.MessageRequest;
 import com.messaging_project.messageservice.services.dtos.responses.MessageResponse;
-import com.messaging_project.messageservice.services.mappers.MessageMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/messages")
 public class MessageController {
     private final MessageService messageService;
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public MessageResponse sendMessageViaWebSocket(@Payload MessageRequest messageRequest) {
+        // WebSocket üzerinden gelen mesajı veritabanına kaydedelim
+        return messageService.sendMessage(messageRequest);
 
-    @PostMapping
-    public MessageResponse sendMessage(@RequestBody MessageRequest requestDTO) {
-        return messageService.sendMessage(requestDTO);
     }
-
     @GetMapping("/conversation")
     public List<MessageResponse> getConversation(@RequestParam int senderId, @RequestParam int receiverId) {
         return messageService.getConversation(senderId, receiverId);
